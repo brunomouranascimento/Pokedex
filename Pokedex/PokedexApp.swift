d96125b94fff4159c13633d13d3e5bd3edf7a831
Bruno Nascimento <bruno.bmn@gmail.com>
2022-04-19 14:48:47 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Bruno Nascimento on 19/04/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
